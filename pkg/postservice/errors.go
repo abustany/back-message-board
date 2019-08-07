dd586a2fc0e9c8748cce7411fe831674ab51b20b
Adrien Bustany <adrien@bustany.org>
2019-08-07 14:48:51 +0200

    postservice: Allow retrieving the user error from an error

--- pkg/postservice/errors.go
+++ pkg/postservice/errors.go
@@ -17,21 +17,29 @@ func (e *userError) Error() string {
 // Type assertion to make sure we implement error correctly...
 var _ error = &userError{}
 
+// IsUserError checks if the given error is flagged as an error caused by a
+// malformed user input or if it is a "normal" runtime error.
 func IsUserError(e error) bool {
+	return UserError(e) != nil
+}
+
+// UserError returns the user error inside the given error if any, or nil if e
+// is nil or not a user error.
+func UserError(e error) error {
 	for e != nil {
-		if _, ok := e.(*userError); ok {
-			return true
+		if uerror, ok := e.(*userError); ok {
+			return uerror.err
 		}
 
 		cause := errors.Cause(e)
 
 		if cause == e {
 			// e didn't implement errors.causer
-			return false
+			return nil
 		}
 
 		e = cause
 	}
 
-	return false
+	return nil
 }
